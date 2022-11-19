6c07e98c29455d85dcd484868d28c24c9a49ba32
MertArtun <86188037+MertArtun@users.noreply.github.com>
2022-11-19 14:44:08 +0300

    Add files via upload

---
+++ MinMaks/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner getValue=new Scanner(System.in);
+        int value,minValue=0,maxValue=0,count,i=1;
+        System.out.print("PHow many numbers will you enter: ");
+        count=getValue.nextInt();
+        while(i!=(count+1)){
+            System.out.print("Please enter "+i+". number: ");
+            if(i==1){
+                minValue=getValue.nextInt();
+            }
+            else {
+                value=getValue.nextInt();
+                if(value<minValue){
+                    minValue=value;
+                }else maxValue=value;
+            }
+            i++;
+
+        }
+        System.out.println("Smallest value is: "+minValue);
+        System.out.println("Largest value is: "+maxValue);
+
+    }
+}
